4560f803d1b04c5768ff76f7bffdf5ad5c6228c4
ayushi16055 <112268459+ayushi16055@users.noreply.github.com>
2023-11-24 14:55:35 +0530

    use mysql as db

--- signup_process.php
+++ signup_process.php
@@ -14,11 +14,32 @@ if ($recaptcha_data->success) {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    // Perform additional validation and database operations if needed
+    // Establish a connection to the MySQL database
+    $servername = "localhost"; // Change this to your MySQL server name if different
+    $username = "root"; // Change this to your MySQL username
+    $password_db = ""; // Change this to your MySQL password if set
+    $dbname = "mysql"; // Change this to your database name
 
-    // For demonstration purposes, redirecting to a homepage dashboard and displaying the user's full name
-    header("Location: dashboard.php?fullname=$fullname");
-    exit();
+    // Create connection
+    $conn = new mysqli($servername, $username, $password_db, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare and execute an SQL statement to insert user data into a table
+    $sql = "INSERT INTO users (fullname, email, password) VALUES ('$fullname', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        // Redirect to a homepage dashboard and pass the user's full name as a parameter
+        header("Location: dashboard.php?fullname=$fullname");
+        exit();
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close(); // Close the database connection
 } else {
     // If reCAPTCHA validation fails, handle the error accordingly
     echo "reCAPTCHA verification failed.";
